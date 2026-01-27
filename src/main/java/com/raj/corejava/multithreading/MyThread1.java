c97511a6a35c061f22405a1841c6ee795bbd21a0
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-01-27 13:40:19 +0530

    MultiThreading

---
+++ src/main/java/com/raj/corejava/multithreading/MyThread1.java
@@ -0,0 +1,25 @@
+package com.raj.corejava.multithreading;
+
+public class MyThread1 extends Thread{
+    String Task;
+
+    MyThread1(String Task) {
+        this.Task = Task;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Task);
+    }
+
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1("Hello World");
+        MyThread1 t2 = new MyThread1("Hii there");
+        MyThread1 t3 = new MyThread1("Hello. Raj");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
