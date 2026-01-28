2f9d2ca6d10cfe5aaa97fb7294aca401a86c4b7c
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-01-28 10:48:31 +0530

    MultiThreading

--- src/main/java/com/raj/corejava/multithreading/MyInterface.java
+++ src/main/java/com/raj/corejava/multithreading/MyInterface.java
@@ -1,41 +1,37 @@
 package com.raj.corejava.multithreading;
 
 public class MyInterface {
+
+    public static void printHello() {
+        System.out.println("Hello World!!!");
+    }
+
+    public static void printName() {
+        System.out.println("Hello Raj");
+    }
+
+    public static void printHii() {
+        System.out.println("Hii There");
+    }
+
+
+
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            System.out.println("Hii There");
-        });
+        Runnable r1 = () -> {
+            printHello();
+        };
 
-        Thread t2 = new Thread(() -> {
-            System.out.println("Hello World!!!");
-        });
+        Runnable r2 = () -> {
+            printName();
+        };
 
-        Thread t3 = new Thread(() -> {
-            System.out.println("Hello Raj");
-        });
+        Runnable r3 = () -> {
+            printHii();
+        };
 
-        /*
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hii there");
-            }
-        });
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello World!!!");
-            }
-        });
-
-        Thread t3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello, Raj");
-            }
-        });
-        */
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        Thread t3 = new Thread(r3);
 
         t1.start();
         t2.start();
